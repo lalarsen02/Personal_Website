5cd6de2ca255d9e08f8eba967b96d7c8ae3a178d
lalarsen02 <113274357+lalarsen02@users.noreply.github.com>
2024-12-20 06:02:08 -0500

    Bug Testing 5

--- static/js/getprojects.js
+++ static/js/getprojects.js
@@ -7,57 +7,58 @@
 
 "use strict";
 
-function handleResponse(response) {
-    // Insert the new HTML
-    $("#resultsDiv").html(response);
+// function handleResponse(response) {
+//     // Insert the new HTML
+//     $("#resultsDiv").html(response);
 
-    // Apply fade-in setup to newly inserted elements
-    const newElements = $("#resultsDiv").find("h3, img");
-    newElements.addClass("fade-in-up");
+//     // Apply fade-in setup to newly inserted elements
+//     const newElements = $("#resultsDiv").find("h3, img");
+//     newElements.addClass("fade-in-up");
 
-    // Scroll-based visibility logic
-    const revealElements = () => {
-        const elements = document.querySelectorAll("#resultsDiv .fade-in-up");
-        elements.forEach((el) => {
-            const rect = el.getBoundingClientRect();
-            if (rect.top < window.innerHeight) {
-                el.classList.add("visible");
-            }
-        });
-    };
+//     // Scroll-based visibility logic
+//     const revealElements = () => {
+//         const elements = document.querySelectorAll("#resultsDiv .fade-in-up");
+//         elements.forEach((el) => {
+//             const rect = el.getBoundingClientRect();
+//             if (rect.top < window.innerHeight) {
+//                 el.classList.add("visible");
+//             }
+//         });
+//     };
 
-    // Add scroll listener and trigger the check immediately
-    window.addEventListener("scroll", revealElements);
-    revealElements();
-}
+//     // Add scroll listener and trigger the check immediately
+//     window.addEventListener("scroll", revealElements);
+//     revealElements();
+// }
 
-let request = null;
+// let request = null;
 
-function getResults(sort = 'recent') {
-    console.log("here1", sort)
-    if (request != null) request.abort();
-    console.log("here2", request)
+// function getResults(sort = 'recent') {
+//     console.log("here1", sort)
+//     if (request != null) request.abort();
+//     console.log("here2", request)
 
-    let url = `/getprojects?sort=${sort}`;
+//     let url = `/getprojects?sort=${sort}`;
 
-    request = $.ajax({
-        type: "GET",
-        url: url,
-        success: handleResponse,
-    });
-    console.log(request)
-}
+//     request = $.ajax({
+//         type: "GET",
+//         url: url,
+//         success: handleResponse,
+//     });
+//     console.log(request)
+// }
 
 function setup() {
+    console.log("hello")
     // getResults();
 
-    $("#recent").on("click", function () {
-        getResults('recent');
-    });
+    // $("#recent").on("click", function () {
+    //     getResults('recent');
+    // });
 
-    $("#alpha").on("click", function () {
-        getResults('alpha');
-    });
+    // $("#alpha").on("click", function () {
+    //     getResults('alpha');
+    // });
 }
 
 $("document").ready(setup);
